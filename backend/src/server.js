6dbe61aae7687012ecad24e3043ddf260c20a05e
umaro14 <omarabrahao14@gmail.com>
2025-10-20 20:46:25 +0100

    Ready ofr deploymnet

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
+//http://localhost:3000
 //http://localhost:3000/api/auth/signup
 //http://localhost:3000/api/auth/login
 //http://localhost:3000/api/auth/logout
@@ -12,10 +14,23 @@ import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 const app = express();
+
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000; 
 
 app.use("/api/auth/", authRoutes)
 app.use("/api/messages/", messageRoutes)
+
+//Make ready for Deployment
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    //If none of the above routes is visited then serve the frontend application
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    })
+}
 
 
 
